9d8d15ad651d4540d34508fb5ddbbca2feff9a39
ZCCKING <1054913015@qq.com>
2016-07-15 21:19:21 +0800

    添加发现界面
    添加发现界面

---
+++ MiMi/MiMi/FoundController.h
@@ -0,0 +1,13 @@
+//
+//  FoundController.h
+//  MiMi
+//
+//  Created by 张崇超 on 16/7/15.
+//  Copyright © 2016年 LLQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FoundController : UIViewController
+
+@end
---
+++ MiMi/MiMi/FoundController.m
@@ -0,0 +1,26 @@
+//
+//  FoundController.m
+//  MiMi
+//
+//  Created by 张崇超 on 16/7/15.
+//  Copyright © 2016年 LLQ. All rights reserved.
+//
+
+#import "FoundController.h"
+
+@interface FoundController ()
+
+@end
+
+@implementation FoundController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
